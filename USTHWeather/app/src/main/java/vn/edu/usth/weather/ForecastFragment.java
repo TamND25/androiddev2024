dd3c9df3e5a03c222e25fb1213bbf8cc48fe0386
YukiMakoto25 <tamnd.22bi13400@usth.edu.vn>
2024-08-28 14:35:13 +0700

    Practical 4

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -8,6 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ForecastFragment extends Fragment {
 
@@ -30,8 +34,31 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
+        FrameLayout view = (FrameLayout) inflater.inflate(R.layout.fragment_forecast, container, false);
         view.setBackgroundColor(Color.parseColor("#2000FF00"));
+
+        // Create LinearLayout
+        LinearLayout linearLayout = new LinearLayout(getActivity());
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        // TextView "Thursday"
+        TextView date = new TextView(getActivity());
+        date.setText(R.string.thursday);
+        date.setTextSize(60);
+        date.setGravity(View.TEXT_ALIGNMENT_GRAVITY);
+        date.setTextColor(Color.parseColor("#FF0000"));
+
+        // ImageView
+        ImageView weatherIcon = new ImageView(getActivity());
+        weatherIcon.setImageResource(R.drawable.weather_icon);
+
+        // Add TextView and ImageView to LinearLayout
+        linearLayout.addView(date);
+        linearLayout.addView(weatherIcon);
+
+        // Add LinearLayout to FrameLayout
+        view.addView(linearLayout);
+
         return view;
     }
 
